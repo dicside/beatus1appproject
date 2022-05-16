6966558f4658ec12e1d63f0bb935c55312c2b704
dicside <sh926lee4@gmail.com>
2022-05-16 23:07:55 +0900

    tailwind test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App(){
+  return(
+    <>
+    <div class="m-4 flex justify-between">
+    <div class=" border-opacity-30 h-6 w-6 bg-white border-2 rounded-full border-black flex space-x-4">
+      <div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
-  );
-}
+    <div class="h-6 w-60 rounded-3xl bg-neutral-300">
+        <div>
+          <p class=" text-slate-500 text-xs p-1">트위터 검색</p>
+        </div>
+    </div>
+    <div class="h-6 w-6 rounded-full">
+      <div>
+        <img class="h-6 w-6" scr="https://media.istockphoto.com/vectors/settings-vector-icon-on-transparent-background-settings-icon-vector-id1013501796?s=612x612" alt=""></img>
+      </div>
+    </div>
+    </div>
+    <div class="clear-left h-60 w-100 bg-black">
+      <div class="p-6 content-end pt-44">
+      <p class="font-medium text-white text-xs">이슈 ' 실시간</p>
+      </div>
+    </div>
+    <div class="border-b-2 ">
+      <div class="p-1 font-xs font-semibold">
+      나를 위한 트랜드
+      </div>
+    </div>
+    <div class="border-b-2">
+      <div class="text-xs font-bold p-4">
+        
+      </div>
+    </div>
+    <div class="border-b-2">
+      <div class="text-xs font-bold p-4">
+      
+      </div>
+    </div>
+    <div class="border-b-2">
+      <div class="text-xs font-bold p-4">
+  
+      </div>
+    </div>
+    <div class="border-b-2">
+      <div class="text-xs font-bold p-4">
+  
+      </div>
+    </div>
+    <div class="border-b-2">
+      <div class="text-xs font-bold p-4">
 
-export default App;
+      </div>
+    </div>
+    <div class="border-b-2">
+      <div class="p-1 font-xs text-sky-500">
+      <a href="">더 보기</a>
+      </div>
+    </div>
+    <div class="mt-2">
+      <div class="border-t-2 border-b-2 p-1 font-bold">팔로우 추천</div>
+    </div>
+    <div class="border-b-2 p-3 flex justfy-between">
+      <div class="m-2 p-3 h-60 w-36 border-2 rounded-xl">
+        <div class="p-0 flex justify-end">
+          <img class="h-3 w-3" scr="" alt=""></img>y
+        </div>
+        <div class=" h-20 w-20 ml-5 rounded-full border-2 ">
+          <img class="h-20 w-20" scr="" alt=""></img>
+        </div>
+        <div class="p-2">
+          <div class="flex justify-center">
+          <p class="text-xm "></p>
+          </div>
+          <div class="flex justify-center">
+          <p class="text-xm text-neutral-400"></p>
+          </div>
+        </div>
+        <div class="bg-sky-500 ml-3 mt-20 content-end flex justigy-center rounded-3xl h-6 w-24">
+        <div class="p-0 flex ml-2 justigy-center content-end text-xm font-bold text-white text ">
+          팔로우하기
+        </div>
+        </div>y
+      </div>
+      <div class="m-2 p-3 h-60 w-36 border-2 rounded-xl">
+        <div class="p-0 flex justify-end">
+          <img class="h-3 w-3" scr="" alt=""></img>
+        </div>
+        <div class=" h-20 w-20 ml-5 rounded-full border-2 ">
+          <img class="h-20 w-20" scr="" alt=""></img>
+        </div>
+        <div class="p-2">
+          <div class="flex justify-center">
+          <p class="text-xm "></p>
+          </div>
+          <div class="flex justify-center">
+          <p class="text-xm text-neutral-400"></p>
+          </div>
+        </div>
+        <div class="bg-sky-500 ml-3 mt-20 content-end flex justigy-center rounded-3xl h-6 w-24">
+        <div class="p-0 flex ml-2 justigy-center content-end text-xm font-bold text-white text ">
+          팔로우하기
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="border-b-2 border-t-2">
+      <div class="p-1 font-xs text-sky-500">
+      <a href="">더 보기</a>
+      </div>
+    </div>
+    </>
+    
+)
+}
